d23b6ae2256fd0df1fa68dde4c380edfa03e7d52
Pedro Gomes Antunes <darkenessninjabr@hotmail.com>
2024-01-22 21:41:30 -0300

    Initialize the DirectX 11

--- ApplicationClass.cpp
+++ ApplicationClass.cpp
@@ -2,6 +2,7 @@
 
 ApplicationClass::ApplicationClass()
 {
+	m_Direct3D = 0;
 }
 
 ApplicationClass::ApplicationClass(const ApplicationClass& other)
@@ -15,20 +16,55 @@ ApplicationClass::~ApplicationClass()
 
 bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 {
+	bool result;
+	
+	// Create and initialize the Direct3D object
+	m_Direct3D = new D3DClass;
+
+	result = m_Direct3D->Initialize(screenWidth, screenHeight, VSYNC_ENABLED, hwnd, FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR);
+	if (!result)
+	{
+		MessageBox(hwnd, L"Could not initialize Direct#d", L"Error", MB_OK);
+		return false;
+	}
+
 	return true;
 }
 
 void ApplicationClass::Shutdown()
 {
+	// Release the Direct3D object
+	if (m_Direct3D)
+	{
+		m_Direct3D->Shutdown();
+		delete(m_Direct3D);
+		m_Direct3D = 0;
+	}
+
 	return;
 }
 
 bool ApplicationClass::Frame()
 {
+	bool result;
+	
+	// Render the graphics scene
+	result = Render();
+	if (!result)
+	{
+		return false;
+	}
+
 	return true;
 }
 
 bool ApplicationClass::Render()
 {
+	// Clear the buffers to begin the scene
+	m_Direct3D->BeginScene(1.0f, 0.1f, 0.1f, 1.0f);
+
+	// Present the rendered scene to the screen
+	m_Direct3D->EndScene();
+
 	return true;
 }
