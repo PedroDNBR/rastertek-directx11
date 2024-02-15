a4ac8b76da3d5492db011f502a7e9150345a00a4
Pedro Gomes Antunes <darkenessninjabr@hotmail.com>
2024-02-14 23:18:19 -0300

    Change the triangle to a square

--- ModelClass.cpp
+++ ModelClass.cpp
@@ -58,10 +58,10 @@ bool ModelClass::InitializeBuffers(ID3D11Device* device)
 	HRESULT result;
 
 	// Set the number of vertices in the vertex array
-	m_vertexCount = 3;
+	m_vertexCount = 6;
 
 	// Set the number of indices in the index array
-	m_indexCount = 3;
+	m_indexCount = 6;
 
 	// Create the vertex array
 	vertices = new VertexType[m_vertexCount];
@@ -81,16 +81,29 @@ bool ModelClass::InitializeBuffers(ID3D11Device* device)
 	vertices[0].position = XMFLOAT3(-1.0f, -1.0f, 0.0f); // Bottom left
 	vertices[0].color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
 
-	vertices[1].position = XMFLOAT3(0.0f, 1.0f, 0.0f); // Top middle
+	vertices[1].position = XMFLOAT3(-1.0f, 1.0f, 0.0f); // Top left
 	vertices[1].color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
 
-	vertices[2].position = XMFLOAT3(1.0f, -1.0f, 0.0f); // Bottom right
+	vertices[2].position = XMFLOAT3(1.0f, 1.0f, 0.0f); // Top right
 	vertices[2].color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
+
+	vertices[3].position = XMFLOAT3(-1.0f, -1.0f, 0.0f);  // Bottom left
+	vertices[3].color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
+
+	vertices[4].position = XMFLOAT3(1.0f, 1.0f, 0.0f);  // Top right
+	vertices[4].color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
+
+	vertices[5].position = XMFLOAT3(1.0f, -1.0f, 0.0f);  // Bottom right
+	vertices[5].color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
 
 	// Load the index array with data
 	indices[0] = 0; // Bottom left
-	indices[1] = 1; // Top middle
-	indices[2] = 2; // Bottom right
+	indices[1] = 1; // Top left
+	indices[2] = 2; // top right
+
+	indices[3] = 3; // Bottom left
+	indices[4] = 4; // Top left
+	indices[5] = 5; // top right
 
 	// Set up the description of the static vertex buffer
 	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
