f711e8bb56ea2c73ede7cdd024612789f9fcab28
Pedro Gomes Antunes <darkenessninjabr@hotmail.com>
2024-02-18 21:38:52 -0300

    Texturing the Triangle

--- ApplicationClass.h
+++ ApplicationClass.h
@@ -4,7 +4,7 @@
 #include "D3DClass.h"
 #include "CameraClass.h"
 #include "ModelClass.h"
-#include "ColorShaderClass.h"
+#include "TextureShaderClass.h"
 
 const bool FULL_SCREEN = false;
 const bool VSYNC_ENABLED = true;
@@ -27,7 +27,7 @@ private:
 	D3DClass* m_Direct3D;
 	CameraClass* m_Camera;
 	ModelClass* m_Model;
-	ColorShaderClass* m_ColorShader;
+	TextureShaderClass* m_TextureShader;
 
 };
 
--- ModelClass.cpp
+++ ModelClass.cpp
@@ -4,6 +4,7 @@ ModelClass::ModelClass()
 {
 	m_vertexBuffer = 0;
 	m_indexBuffer = 0;
+	m_Texture = 0;
 }
 
 ModelClass::ModelClass(const ModelClass &other)
@@ -14,7 +15,7 @@ ModelClass::~ModelClass()
 {
 }
 
-bool ModelClass::Initialize(ID3D11Device* device)
+bool ModelClass::Initialize(ID3D11Device* device, ID3D11DeviceContext* deviceContext, char* textureFilename)
 {
 	bool result;
 
@@ -25,11 +26,21 @@ bool ModelClass::Initialize(ID3D11Device* device)
 		return false;
 	}
 
+	// Load the texture for this model
+	result = LoadTexture(device, deviceContext, textureFilename);
+	if (!result)
+	{
+		return false;
+	}
+
 	return true;
 }
 
 void ModelClass::Shutdown()
 {
+	// Release the model texture
+	ReleaseTexture();
+
 	// Shutdown the vertex and index buffers
 	ShutdownBuffers();
 
@@ -49,6 +60,11 @@ int ModelClass::GetIndexCount()
 	return m_indexCount;
 }
 
+ID3D11ShaderResourceView* ModelClass::GetTexture()
+{
+	return m_Texture->GetTexture();
+}
+
 bool ModelClass::InitializeBuffers(ID3D11Device* device)
 {
 	VertexType* vertices;
@@ -58,10 +74,10 @@ bool ModelClass::InitializeBuffers(ID3D11Device* device)
 	HRESULT result;
 
 	// Set the number of vertices in the vertex array
-	m_vertexCount = 6;
+	m_vertexCount = 3;
 
 	// Set the number of indices in the index array
-	m_indexCount = 6;
+	m_indexCount = 3;
 
 	// Create the vertex array
 	vertices = new VertexType[m_vertexCount];
@@ -79,31 +95,19 @@ bool ModelClass::InitializeBuffers(ID3D11Device* device)
 
 	// Load the vertex array with data
 	vertices[0].position = XMFLOAT3(-1.0f, -1.0f, 0.0f); // Bottom left
-	vertices[0].color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
+	vertices[0].texture = XMFLOAT2(0.0f, 1.0f);
 
-	vertices[1].position = XMFLOAT3(-1.0f, 1.0f, 0.0f); // Top left
-	vertices[1].color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
+	vertices[1].position = XMFLOAT3(0.0f, 1.0f, 0.0f); // Top middle
+	vertices[1].texture = XMFLOAT2(0.5f, 0.0f);
 
-	vertices[2].position = XMFLOAT3(1.0f, 1.0f, 0.0f); // Top right
-	vertices[2].color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
+	vertices[2].position = XMFLOAT3(1.0f, -1.0f, 0.0f); // Bottom right
+	vertices[2].texture = XMFLOAT2(1.0f, 1.0f);
 
-	vertices[3].position = XMFLOAT3(-1.0f, -1.0f, 0.0f);  // Bottom left
-	vertices[3].color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
-
-	vertices[4].position = XMFLOAT3(1.0f, 1.0f, 0.0f);  // Top right
-	vertices[4].color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
-
-	vertices[5].position = XMFLOAT3(1.0f, -1.0f, 0.0f);  // Bottom right
-	vertices[5].color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
 
 	// Load the index array with data
 	indices[0] = 0; // Bottom left
-	indices[1] = 1; // Top left
-	indices[2] = 2; // top right
-
-	indices[3] = 3; // Bottom left
-	indices[4] = 4; // Top left
-	indices[5] = 5; // top right
+	indices[1] = 1; // Top middle
+	indices[2] = 2; // Bottom right
 
 	// Set up the description of the static vertex buffer
 	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
@@ -194,3 +198,34 @@ void ModelClass::RenderBuffers(ID3D11DeviceContext* deviceContext)
 
 	return;
 }
+
+bool ModelClass::LoadTexture(ID3D11Device* device, ID3D11DeviceContext* deviceContext, char* filename)
+{
+	bool result;
+
+	// Create and initialize the texture object
+	m_Texture = new TextureClass;
+
+	result = m_Texture->Initialize(device, deviceContext, filename);
+	if (!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void ModelClass::ReleaseTexture()
+{
+	// Release the texture object
+	if (m_Texture)
+	{
+		m_Texture->Shutdown();
+		delete m_Texture;
+		m_Texture = 0;
+	}
+
+	return;
+}
+
+
