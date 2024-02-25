a48224bddb02244d6d57ea698edf20ea65a60dc7
Pedro Gomes Antunes <darkenessninjabr@hotmail.com>
2024-02-25 15:40:32 -0300

    3D Model Rendering

--- ApplicationClass.cpp
+++ ApplicationClass.cpp
@@ -20,6 +20,7 @@ ApplicationClass::~ApplicationClass()
 
 bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 {
+	char modelFilename[128];
 	char textureFilename[128];
 	bool result;
 	
@@ -39,13 +40,16 @@ bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 	// Set the initial position of the camera
 	m_Camera->SetPosition(0.0f, 0.0f, -5.0f);
 
-	// Create and initialize the model object
-	m_Model = new ModelClass;
+	// Set the file name of the model
+	strcpy_s(modelFilename, "./Cube.txt");
 
 	// Set the name of the texture file that we will be loading
 	strcpy_s(textureFilename, "./stone01.tga");
 
-	result = m_Model->Initialize(m_Direct3D->GetDevice(), m_Direct3D->GetDeviceContext(), textureFilename);
+	// Create and initialize the model object
+	m_Model = new ModelClass;
+
+	result = m_Model->Initialize(m_Direct3D->GetDevice(), m_Direct3D->GetDeviceContext(), modelFilename, textureFilename);
 	if (!result)
 	{
 		MessageBox(hwnd, L"Cound not initialize the model object", L"Error", MB_OK);
--- ModelClass.h
+++ ModelClass.h
@@ -3,7 +3,9 @@
 
 #include <d3d11.h>
 #include <DirectXMath.h>
+#include <fstream>
 using namespace DirectX;
+using namespace std;
 
 #include "TextureClass.h"
 
@@ -17,12 +19,19 @@ private:
 		XMFLOAT3 normal;
 	};
 
+	struct ModelType
+	{
+		float x, y, z;
+		float tu, tv;
+		float nx, ny, nz;
+	};
+
 public:
 	ModelClass();
 	ModelClass(const ModelClass&);
 	~ModelClass();
 
-	bool Initialize(ID3D11Device*, ID3D11DeviceContext*, char*);
+	bool Initialize(ID3D11Device*, ID3D11DeviceContext*, char*, char*);
 	void Shutdown();
 	void Render(ID3D11DeviceContext*);
 
@@ -38,11 +47,14 @@ private:
 	bool LoadTexture(ID3D11Device*, ID3D11DeviceContext*, char*);
 	void ReleaseTexture();
 
+	bool LoadModel(char*);
+	void ReleaseModel();
+
 	ID3D11Buffer* m_vertexBuffer, * m_indexBuffer;
 	int m_vertexCount, m_indexCount;
 
 	TextureClass* m_Texture;
-
+	ModelType* m_model;
 };
 
 #endif
